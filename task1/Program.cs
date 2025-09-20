e7e18005e40b740cd0ea50f255effab80f7cbb34
Irina894 <irinaderevoriz@gmail.com>
2025-09-20 14:43:08 +0300

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;   
+
+class Person
+    {
+    public string Name;
+    public int Age;
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old.");
+        }
+}
+public class Program
+    {
+       public static void Main()
+        {
+           Person person = new Person("Alice", 30);
+        person.Show();
+        Person person2 = new Person("Bob", 25);
+        person2.Show();
+        Person person3 = new Person("Petro", 15);
+        person3.Show();
+
+    }
+    }
+
